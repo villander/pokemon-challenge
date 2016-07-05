5a87f06732f745ad06e367abbadc2c975bedeb5b
Michael Villander <nousvillander@gmail.com>
2016-07-05 18:48:05 -0300

    Add unit test to models with relationship

--- tests/unit/models/comment-test.js
+++ tests/unit/models/comment-test.js
@@ -1,24 +1,21 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+
+const {
+  get
+} = Ember;
 
 moduleForModel('comment', 'Unit | Model | comment', {
   // Specify the other units that are required for this test.
   needs: ['model:pokemon']
 });
 
-test('it has the attributes: name, email, pokemon, message, createAt', function(assert) {
-  assert.expect(5);
+test('shoud has a relationship with pokemon', function(assert) {
+  assert.expect(2);
 
-  const model = this.subject();
+  const model = this.store().modelFor('comment');
+  const relationship = get(model, 'relationshipsByName').get('pokemon');
 
-  const hasAttrName = Object.keys(model.toJSON()).indexOf('name') > -1;
-  const hasAttrEmail = Object.keys(model.toJSON()).indexOf('email') > -1;
-  const hasAttrPokemon = Object.keys(model.toJSON()).indexOf('pokemon') > -1;
-  const hasAttrMessage = Object.keys(model.toJSON()).indexOf('message') > -1;
-  const hasAttrCreatedAt = Object.keys(model.toJSON()).indexOf('createdAt') > -1;
-
-  assert.ok(hasAttrName);
-  assert.ok(hasAttrEmail);
-  assert.ok(hasAttrPokemon);
-  assert.ok(hasAttrMessage);
-  assert.ok(hasAttrCreatedAt);
+  assert.equal(relationship.key, 'pokemon', 'has relationship with pokemon');
+  assert.equal(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
 });
--- tests/unit/models/pokemon-test.js
+++ tests/unit/models/pokemon-test.js
@@ -1,12 +1,23 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+
+const {
+  get
+} = Ember;
 
 moduleForModel('pokemon', 'Unit | Model | pokemon', {
   // Specify the other units that are required for this test.
   needs: ['model:comment']
 });
 
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+
+test('shoud has a relationship with comment', function(assert) {
+  assert.expect(2);
+
+  const model = this.store().modelFor('pokemon');
+  const relationship = get(model, 'relationshipsByName').get('comments');
+  console.log(relationship.key);
+
+  assert.equal(relationship.key, 'comments', 'has relationship with comment');
+  assert.equal(relationship.kind, 'hasMany', 'kind of relationship is hasMany');
 });
